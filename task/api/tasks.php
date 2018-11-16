5c307c1f29e31317f89d02218f49789768a47f4f
uca <uca>
2018-11-16 14:59:17 -0600

    labo11

---
+++ task/api/tasks.php
@@ -0,0 +1,8 @@
+<?php
+                /*Este archivo nos devolvera la lista de tareas*/
+
+                require('../dao/TaskDao.php');
+                $dao = new TaskDao();
+                http_response_code(200);
+                echo $dao->readAll();
+              ?>
